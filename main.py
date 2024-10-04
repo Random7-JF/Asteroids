f9a893ebf75d86f74dfd5a9b7f84117fd30c443e
Jon Fingas <jon.fingas@gmail.com>
2024-10-03 23:13:37 -0500

    switched to how the lesson did it

--- main.py
+++ main.py
@@ -3,16 +3,13 @@ from constants import *
 
 def main():
     pygame.init()
-    window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print("Screen width:", SCREEN_WIDTH)
-    print("Screen height:", SCREEN_HEIGHT)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(window, color=(0,0,0))
+        screen.fill("black")
         pygame.display.flip()
 
 if __name__ == "__main__":
